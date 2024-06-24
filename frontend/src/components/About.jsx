6fba023132bbb8e532e085a5731f9996122f6a0b
Gopika <kvgopika3@gmail.com>
2024-06-24 14:28:58 +0100

    Initial front and backend adding

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>
+      "The value of love is easily corrupted by pain, and division often results from enduring this conflict. Balancing the tangible and intangible, one may despise the challenges of new beginnings. Innovation and truth can lead to exceptional accusations and annoying choices, yet they are vital to progress. Souls striving for achievement face official scrutiny, and the blame for present difficulties is hard to escape. This recalls a bittersweet reality. Accusations and choices highlight the complexities of will and responsibility, illuminating the burdens we carry and the names we honor. In essence, the struggle to maintain integrity in the face of adversity is universal."
+      </p>
+    </section>
+  );
+};
+
+export default About;
